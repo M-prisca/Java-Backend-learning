7b8cfa9800f2edaac909cc5634c4d2d80a20e0e9
Prisca <priscamasereli@gmail.com>
2026-03-10 11:05:53 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 21;
+        String name = "Prisca";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+
+}
