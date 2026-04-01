c816f4578f737b4e698c03185e733c955245fa6f
Prisca <priscamasereli@gmail.com>
2026-04-01 16:31:44 +0200

    feat: Mental Models

---
+++ Mental Models/Week3-exercises/Prog.java
@@ -0,0 +1,18 @@
+public class Prog {
+    public static void main(String[] args) {
+        Helper.run();
+    }
+
+}
+
+class Helper {
+    static void run() {
+        System.out.println("Hello from multiple files");
+    }
+
+}
+
+// Question: while using the method of running multiple files at the same time,
+// for example if you have two files, when you've created the first one as a
+// public class why can't you create the second one also with the public class
+// keyword?
---
+++ Mental Models/week2-exercises/CaesarCipherDecoder.java
@@ -0,0 +1,29 @@
+// Exercise 2: Caesar Cipher Decoder
+// You intercepted a coded message where every character was shifted forward by 3 letters.
+
+// Example:
+
+// Encoded: khoor
+// Original: hello
+// Tasks
+// Go through each character in the string.
+// If it is not a space(spaces remain the same), shift it 3 characters backward
+// Build the decoded message.
+// Print the result.
+// Expected Output
+// Encoded message: khoor zruog
+// Decoded message: hello world
+// Length: 11
+
+public class CaesarCipherDecoder {
+    public static void main(String[] args) {
+        String encoded = "khoor";
+        String decoded = "";
+
+        for (int i = 0; i < encoded.length(); i++) {
+
+        }
+
+    }
+
+}
