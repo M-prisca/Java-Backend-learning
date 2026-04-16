fb782ac6f72ca4f3956c63071fd4533a434db9b5
Prisca <priscamasereli@gmail.com>
2026-04-16 09:08:19 +0200

    feat: add Game character exercise

---
+++ src/main/java/learning/javabasics/Typesandvariables/GameCharacter.java
@@ -0,0 +1,11 @@
+public class GameCharacter {
+
+    public static void main(String[] args) {
+        String characterName = "Hero";
+        int healthPoints = 100;
+        double experienceLevel = 1.5;
+        boolean tutorialCompleted = false;
+        System.out.println("\nCharacter: " + characterName + "\nHP: " + healthPoints + "\nLevel: " + experienceLevel
+                + "\nTutorial Completed: " + tutorialCompleted + "\n");
+    }
+}
