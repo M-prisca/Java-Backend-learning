8ce1f5dbe8b886529b9808ece71d0ffef878c817
Prisca <priscamasereli@gmail.com>
2026-04-20 21:33:50 +0200

    feat: first regex exercise

---
+++ src/main/java/learning/Advance/regex/MatcherPattern.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatcherPattern {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("agent\\d\\d\\d");
+        Matcher matcher = pattern.matcher("agent987");
+
+        boolean found = matcher.find();
+        System.out.println("found: " + found);
+
+    }
+}
+
+// Working with regex a bit
